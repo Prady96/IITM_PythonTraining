0106a5ef2ecba1498e56c4349b6400a160c9b28f
Pradyum Gupta <30495649+Prady96@users.noreply.github.com>
2018-12-30 22:39:54 +0530

    Update epoch 30

--- develop/python_model_scripts/custom.py
+++ develop/python_model_scripts/custom.py
@@ -195,7 +195,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=20,
+                epochs=30,
                 layers='heads')
 
 
