b392e0a76a8ea5f98db2ae782375e27f34e216ca
Pradyum Gupta <30495649+Prady96@users.noreply.github.com>
2018-12-30 20:16:08 +0530

    update epochs 20 for collab

--- develop/python_model_scripts/custom.py
+++ develop/python_model_scripts/custom.py
@@ -195,7 +195,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=10,
+                epochs=20,
                 layers='heads')
 
 
