239cd389d5f1debcc25c175f511dab73057f6b31
Pradyum Gupta <pradyumg@gmail.com>
2019-04-01 13:16:00 +0530

    stack datastructure

---
+++ dataStructure/stack.py
@@ -0,0 +1,14 @@
+class Stack():
+    def __init__(self):
+        self.items = [] #empty list
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def get_stack(self):
+        return self.items
+    def is_empty(self):
+        return self.items == []
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
