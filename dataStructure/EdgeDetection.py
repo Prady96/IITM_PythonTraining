7ced699a73ac07337b408aeb7d2b1be7a2a017cc
Pradyum Gupta <pradyumg@gmail.com>
2019-04-10 22:38:10 +0000

    upadte

--- dataStructure/EdgeDetection.py
+++ dataStructure/EdgeDetection.py
@@ -20,6 +20,64 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import sys
 
+def make_cordinates(image, line_parameters):
+    """
+    y = mx + c
+    y,x --> cordinates
+    m --> slope
+    c --> intercept
+
+    here we have slope and intercept
+
+    Now for the cordinates formula is
+    y = mx + c
+    x = (-c + y)/m
+    x = (y - intercept)/slope
+    ## final cordinates (x,y) from slope and intercept
+    """
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
+def averaged_slope_intercept(image, lines):
+    """
+    We are using np.polyfit it returns two parematers
+    1. Slope
+    2. Intercept
+
+    From which -ve slope added to left_fit
+                +ve slope added to right_fit
+    """
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)  ## convert 2d into 1d
+        paramters = np.polyfit((x1,x2), (y1, y2), 1)
+        slope = paramters[0]
+        intercept = paramters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    #average left & right
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    #cordinates
+    left_line = make_cordinates(image, left_fit_average)
+    right_line = make_cordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4) #convert 2d array into 1d array
+            cv2.line(line_image, (x1,y1), (x2,y2), (255,0,0), 10)
+    return line_image
+
 def region_of_interest(image):
     """
     This will depict the masked area from the image
@@ -35,19 +93,68 @@ def region_of_interest(image):
 
 
 def Canny(image):
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5),0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
-    # print(x[:1])
-image = cv2.imread(path)
-cv2.imshow('original Image', image)
-lane_image = np.copy(image)
-canny = Canny(lane_image)
-cropped_image = region_of_interest(canny)
-lines = cv2.HoughLinesP(cropped_image, )
-cv2.imshow('result of region of interest', cropped_image)
-cv2.waitKey(0)
+
+# image = cv2.imread(path)
+# cv2.imshow('original Image', image)
+# lane_image = np.copy(image)
+
+
+######### Video Capture ################
+
+videoPath = '/home/pradyum/The Complete Self-Driving Car Course - Applied Deep Learning/5. Computer Vision Finding Lane-Lines/13.1 test2.mp4.mp4'
+cap = cv2.VideoCapture(videoPath)
+while(cap.isOpened()):
+    _,frame = cap.read()
+    canny_image = Canny(frame)
+    cropped_image = region_of_interest(canny_image)
+
+    ## 2px with 1 degree precision
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180,
+                            100, np.array([]), minLineLength=40,
+                            maxLineGap=5)
+    averaged_lines = averaged_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
+    ## blending both the image
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    # cv2.imshow('result of region of interest', line_image)
+    cv2.imshow("Combo image", combo_image)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+cap.release()
+cv2.distroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -76,3 +183,4 @@ cv2.waitKey(0)
 
 # plt.imshow(canny)
 # plt.show()
+    # print(x[:1])
---
+++ dataStructure/shapeDetection.py
@@ -0,0 +1,6 @@
+import cv2
+
+class ShapeDetector:
+    def __init__(self):
+        pass
+    
